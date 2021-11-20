f8503126bb909afc3e14b71ae26d079c1877ede0
daniel <cheese@gaf.com>
2021-11-20 13:45:10 -0500

    added search bar feature

---
+++ PleaseWorkSearch/PleaseWorkSearch/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace PleaseWorkSearch
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(395, 215);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 94);
+            this.listBox1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(395, 142);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(120, 23);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ PleaseWorkSearch/PleaseWorkSearch/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PleaseWorkSearch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            listcollection.Clear();
+            string[] listItems = { "league.exe", "chrome.exe", "sus.exe", "bruh.exe", "bruhchungus.exe", "brick.exe", "f" };
+            foreach (string app in listItems)
+            {
+                listBox1.Items.Add(app);
+            }
+            foreach (string str in listBox1.Items)
+            {
+                listcollection.Add(str);
+                
+            }
+
+        }
+
+        List<string> listcollection = new List<string>();
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox1.Text) == false)
+            {
+                listBox1.Items.Clear();
+                foreach (string str in listcollection)
+                {
+                    
+                    if (str.StartsWith(textBox1.Text))
+                    {
+                        listBox1.Items.Add(str);
+                    }
+                }
+            }
+            else if (textBox1.Text == "")
+            {
+                listBox1.Items.Clear();
+                foreach (string str in listcollection)
+                {
+                    listBox1.Items.Add(str);
+                }
+            }
+
+        }
+    }
+}
